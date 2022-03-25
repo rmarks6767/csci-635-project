8ca0dbe0161c7b3a7d348c4953b78a61be4fe283
River Marks <rmarks6767@gmail.com>
2022-03-25 13:40:05 -0400

    Finished up the data concatenator

--- read_all_data.py
+++ read_all_data.py
@@ -1,64 +1,61 @@
-import pandas as pd
 import numpy as np
 from numpy import genfromtxt
-import matplotlib.pyplot as plt
 from mnist import MNIST
 import os
 from skimage.io import imread
 import numpy as np
-import random
 
+'''
+  Helper function to read all of the data into one giant array so
+  we can run it through our training algorithm
+'''
 def read_all_data():
   # Get the data and labels for the arabic numerals
+  print('READING ARABIC DATA')
   arabic_train_data = genfromtxt("./data/arabic/csvTrainImages 60k x 784.csv", delimiter=',')
   arabic_test_data = genfromtxt("./data/arabic/csvTestImages 10k x 784.csv", delimiter=',')
   arabic_train_label = genfromtxt("./data/arabic/csvTrainLabel 60k x 1.csv", delimiter=',')
   arabic_test_label = genfromtxt("./data/arabic/csvTestLabel 10k x 1.csv", delimiter=',')
 
-  print(len(arabic_train_data))
-  print(len(arabic_train_data[0]))
-
   # We will split these apart later
   arabic_data = np.concatenate((arabic_train_data, arabic_test_data))
   arabic_label = np.concatenate((arabic_train_label, arabic_test_label))
 
+  print('FINISHED READING ARABIC DATA')
+
   # Get the data and labels for the mnist
+  print('READING THE ENGLISH DATA')
   mndata = MNIST(os.path.join(os.path.dirname(__file__), 'data/english'))
-  english_data, english_label = mndata.load_training()
-  english_data = np.array(english_data)
-  english_label = np.array(english_label)
+  english_train_data, english_train_label = mndata.load_training()
+  english_test_data, english_test_label = mndata.load_testing()
 
+  english_data = np.concatenate((english_train_data, english_test_data))
+  english_label = np.concatenate((english_train_label, english_test_label))
 
-  index = random.randrange(0, len(english_data))  # choose an index ;-)
-  print(mndata.display(english_data[index]))
+  print('FINISHED READING ENGLISH DATA')
 
   # Get the data and labels for the Sylheti
+  print('READING THE SYLHETI DATA')
   sylheti_data = []
   sylheti_label = []
 
   for img in os.listdir('./data/sylheti'):
     # Read in the images
     _, label = img.replace('.png', '').split('_')
-    image = imread(os.path.join('data/sylheti', img))
-
-    # Resize it to be the appropriate size (should already be this for ours)
-    # im_resized = resize(image, (28, 28))
+    image = imread(os.path.join('data/sylheti', img), as_gray=True)
 
     # Append them to our images and labels array
-    sylheti_data.append(image)
+    sylheti_data.append(image.flatten())
     sylheti_label.append(label)
-  
-  print(len(arabic_data))
-  print(len(arabic_data[0]))
-  print(len(english_data))
-  print(len(english_data[0]))
-  # print(len(sylheti_data))
-  # print(len(sylheti_data[0]))
+
+  print('FINISHED READING SYLHETI DATA')
 
   # Combine all of them into one data source
   all_data = np.concatenate((arabic_data, english_data, sylheti_data))
   all_label = np.concatenate((arabic_label, english_label, sylheti_label))
 
-  return np.array(all_data), np.array(all_label)
+  print(f'TOTAL EXAMPLES LOADED: {len(all_data)}')
+
+  return all_data, all_label
 
 read_all_data()
