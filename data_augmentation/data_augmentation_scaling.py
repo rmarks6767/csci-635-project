32199b9d19bdc90234a2a55bee0caee061f7c610
fabienpre <fabien.pre@gmail.com>
2022-03-31 00:15:40 +0200

    Created a function to zoom or unzoom an image

--- data_augmentation/data_augmentation_scaling.py
+++ data_augmentation/data_augmentation_scaling.py
@@ -1,3 +1,4 @@
+from typing import final
 from PIL import Image
 import os
 import numpy as np
@@ -5,37 +6,24 @@ import skimage
 import cv2
 
 
-img = cv2.imread('./data/sylheti/0_0.png')
+def zoom(path,enlargement):
+    img = cv2.imread(path)
 
-width = int(img.shape[1] * 1.5)
-height = int(img.shape[0] * 1.5)
-dim = (width, height)
+    width = int(img.shape[1] * enlargement)
+    height = int(img.shape[0] * enlargement)
+    dim = (width, height)
 
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
-resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    if enlargement > 1:
+        final_img = resized[np.abs(28 - width)//2:np.abs(28 + height)//2, np.abs(28 - width)//2:np.abs(28 + height)//2]
 
+    else:
+        final_img = np.zeros((28, 28, 3))
+        final_img[np.abs(28 - width)//2:np.abs(28 + height)//2, np.abs(28 - width)//2:np.abs(28 + height)//2] = resized
 
-# # Scikit Image. 
-# img = Image.open('./data/sylheti/0_0.png')
+    return(final_img)
 
-# img_img = Image.fromarray(img.astype(np.uint8))
-# img_img.show()
-
-# #print(np.shape(img))
-
-# # For details about 'mode', checkout the interpolation section.
-# scale_out = skimage.transform.rescale(img, scale=1.1, mode='constant')
-# scale_in = skimage.transform.rescale(img, scale=0.9, mode='constant')
-# # Don't forget to crop the images back to the original size (for scale_out)
-
-# #print(np.shape(scale_out))
-# #print(np.shape(scale_in))
-
-# scale_out_img = Image.fromarray(scale_out.astype(np.uint8))
-# #scale_in_img = Image.fromarray(scale_in.astype(np.uint8))
-
-# scale_out_img.show()
-# #scale_in_img.show()
-
-# #print(scale_out_img)
-# #print(scale_in)
+cv2.imshow("sample", zoom('./data/sylheti/0_0.png',0.5))
+#cv2.imshow("sample1", img)
+cv2.waitKey(0)
