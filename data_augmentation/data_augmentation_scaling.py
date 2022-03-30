997cd9843fba81c5fc34f9841ab8ed140d3aff8c
River Marks <rmarks6767@gmail.com>
2022-03-30 16:34:00 -0400

    Image reading

--- data_augmentation/data_augmentation_scaling.py
+++ data_augmentation/data_augmentation_scaling.py
@@ -4,27 +4,38 @@ import numpy as np
 import skimage
 import cv2
 
-# Scikit Image. 
-img = Image.open('C:/Users/fabie/OneDrive/Documents/GitHub/sylheti-training-data/0-0.png')
 
-#img_img = Image.fromarray(img.astype(np.uint8))
-#img_img.show()
+img = cv2.imread('./data/sylheti/0_0.png')
 
-#print(np.shape(img))
+width = int(img.shape[1] * 1.5)
+height = int(img.shape[0] * 1.5)
+dim = (width, height)
 
-# For details about 'mode', checkout the interpolation section.
-scale_out = skimage.transform.rescale(img, scale=1.1, mode='constant')
-scale_in = skimage.transform.rescale(img, scale=0.9, mode='constant')
-# Don't forget to crop the images back to the original size (for scale_out)
 
-#print(np.shape(scale_out))
-#print(np.shape(scale_in))
+resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
-scale_out_img = Image.fromarray(scale_out.astype(np.uint8))
-#scale_in_img = Image.fromarray(scale_in.astype(np.uint8))
 
-scale_out_img.show()
-#scale_in_img.show()
+# # Scikit Image. 
+# img = Image.open('./data/sylheti/0_0.png')
 
-#print(scale_out_img)
-#print(scale_in)
+# img_img = Image.fromarray(img.astype(np.uint8))
+# img_img.show()
+
+# #print(np.shape(img))
+
+# # For details about 'mode', checkout the interpolation section.
+# scale_out = skimage.transform.rescale(img, scale=1.1, mode='constant')
+# scale_in = skimage.transform.rescale(img, scale=0.9, mode='constant')
+# # Don't forget to crop the images back to the original size (for scale_out)
+
+# #print(np.shape(scale_out))
+# #print(np.shape(scale_in))
+
+# scale_out_img = Image.fromarray(scale_out.astype(np.uint8))
+# #scale_in_img = Image.fromarray(scale_in.astype(np.uint8))
+
+# scale_out_img.show()
+# #scale_in_img.show()
+
+# #print(scale_out_img)
+# #print(scale_in)
