1489d027694ffd23aa63b8b2f0cebee5b2f37565
DiamanteDayZ <82218636+DiamanteDayZ@users.noreply.github.com>
2022-04-09 13:30:59 -0400

    Update of rotation and mean files

--- data_augmentation/data_augmentation_rotation.py
+++ data_augmentation/data_augmentation_rotation.py
@@ -1,11 +1,16 @@
 from PIL import Image
 import os
-path=r'C:\Users\vsadr\OneDrive\Documents\GitHub\sylheti-training-data'
-for i in os.listdir(path):
-    data=Image.open(path+'\\' +i)
-    new_data=data.rotate(1)
-    filename='augmented'+i
-    new_data.save(filename,'png')
+import re
+paths=os.path.join(re.sub('data_augmentation','',os.path.dirname(__file__)), 'data\sylheti')
+def rotation(paths):
+    for i in os.listdir(paths):
+        try :
+            data=Image.open(paths+'\\' +i)
+        except:
+            break
+        new_data=data.rotate(1)
+        filename='augmented'+i+'.png'
+        new_data.save(filename,'png')
     
-
+rotation(paths)
 
