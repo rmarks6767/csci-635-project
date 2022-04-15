30c3da17126b4b41bb0bbb73220f40ec7008d7dd
Van Pham <vnp7514@rit.edu>
2022-04-14 23:20:58 -0400

    98% accuracy what????

--- main/models/neural_network_multi_layer_perceptron.py
+++ main/models/neural_network_multi_layer_perceptron.py
@@ -1,87 +1,72 @@
-import os.path
-from sklearn.neural_network import MLPClassifier
-from mnist import MNIST
-from joblib import dump, load
-from data_loader import load_all_data
+import tensorflow as tf
 import numpy as np
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import classification_report
+from main.utils.print import pretty_print_confusion
 
 from main.data.data_loader import load_all_data
 
 
 class MultiLayerPerceptron:
-    def __init__(self, model_filename = 'MLP.joblib'):
-        self.model = MLPClassifier(hidden_layer_sizes=(588, 392, 196),
-                                   activation='relu',
-                                   solver='sgd',
-                                   alpha=0.0001,
-                                   batch_size='auto',
-                                   learning_rate='constant',
-                                   learning_rate_init=0.001,
-                                   random_state=1)
+    def __init__(self, model_filename='MLP.h5', epochs = 5):
+
+        self.epochs = epochs
+        self.model = tf.keras.models.Sequential([
+            tf.keras.Input(shape=784),
+            tf.keras.layers.Dense(588, activation="sigmoid"),
+            tf.keras.layers.Dense(392, activation="sigmoid"),
+            tf.keras.layers.Dense(196, activation="sigmoid"),
+            tf.keras.layers.Dense(20, activation="softmax")
+        ])
+
+        self.loss_func = tf.keras.losses.SparseCategoricalCrossentropy()
+
         self.model_filename = model_filename
 
-    def train(self):
-        # Get the images from the directory
-        (train_images, train_labels), (test_images, test_labels) = load_all_data(True)
-        self.model.fit(train_images, train_labels)
-        dump(self.model, self.model_filename)
 
-        # Run some predictions and see how accurate they are with a confusion matrix
-        predictions = self.model.predict(test_images)
+    def train(self, train_images, train_labels, verbose = True):
+        train_images = train_images / 255.0
 
-        # Output the confusion matrix and model report
-        self.print_results(test_labels, predictions)
+        # Define predictions and loss function outputs
+        predictions = self.model(train_images[:1]).numpy()
+        self.loss_func(train_labels[:1], predictions).numpy()
 
-    def test(self, image, correct_output):
-        # Load the model from the file we saved it to
-        model = load(self.model_filename)
+        # Compile and run the model on our data
+        self.model.compile(optimizer='adam', loss=self.loss_func, metrics=['accuracy'])
+        self.model.fit(train_images, train_labels, epochs=self.epochs, verbose=verbose)
 
-        # Structure the image how the model will read it
-        image = np.array([image])
-
-        # Predict the image
-        prediction = model.predict(image)
-        print(f'{np.argmax(prediction[0])} should be {correct_output}')
-
-    def print_results(self, test_labels, predictions):
-        labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        print("\nConfusion matrix:")
-        print("Labels: {0}\n".format(",".join(labels)))
-        print(confusion_matrix(test_labels, predictions, labels=labels))
-        print("\nClassification report:")
-        print(classification_report(test_labels, predictions))
+        # Save the model so we can use it later
+        self.model.save(f'main/trained_models/{self.model_filename}')
 
 
-def main1():
-    # PLAYING AROUND With ENGLISH DATA
-    (train_data, train_label), (test_data, test_label) = load_all_data(True)
-    # Insight into choosing hidden layers and their nodes:
-    # https://www.linkedin.com/pulse/choosing-number-hidden-layers-neurons-neural-networks-sachdev
-    # 3 hidden layers
-    clf = MLPClassifier(hidden_layer_sizes=(588, 392, 196),
-                        activation='relu',
-                        solver='sgd',
-                        alpha=0.0001,
-                        batch_size='auto',
-                        learning_rate='constant',
-                        learning_rate_init=0.001,
-                        random_state=1)
-    clf.fit(train_data, train_label)
-    prediction = clf.predict(test_data)
-    idx = 0
-    correct = 0
-    total = len(english_test_label)
-    while idx < total:
-        if prediction[idx] == test_label[idx]:
-            correct += 1
-        idx += 1
-    print(correct/total)
+    def evaluate(self, test_images, test_labels, load = False):
+        test_images = test_images / 255.0
+
+        # If we want to load the load we can use the load tag
+        if load:
+            self.model = tf.keras.models.load_model(f'main/trained_models/{self.model_filename}')
+
+        # Run predictions on our test data and evaluate results
+        predictions = []
+        for p in self.model.predict(test_images):
+            predictions.append(np.argmax(p))
+
+        # Create a confusion matrix for our data
+        print('\nConfusion Matrix:')
+        pretty_print_confusion(tf.math.confusion_matrix(test_labels, predictions))
+
+        # Print the classification report
+        print('\nClassification report:')
+        report = classification_report(test_labels, predictions, output_dict=True, zero_division=0)
+        print(classification_report(test_labels, predictions, zero_division=0))
+
+        return report
 
 
 def main():
-    mlp = MultiLayerPerceptron('m1')
-    mlp.train()
+    mlp = MultiLayerPerceptron(model_filename='m1.h5')
+    (train_images, train_labels), (test_images, test_labels) = load_all_data(True)
+    #mlp.train(train_images, train_labels)
+    mlp.evaluate(test_images, test_labels, load=True)
 
 
 if __name__ == "__main__":
