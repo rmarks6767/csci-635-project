865747ab2af5526dde5b3d7d73ef938ac48848ca
Van Pham <vnp7514@rit.edu>
2022-04-14 23:21:59 -0400

    better format

--- main/utils/print.py
+++ main/utils/print.py
@@ -1,18 +1,18 @@
 import sys
-from data.data_loader import labels
+from main.data.data_loader import labels
 
 def pretty_print_confusion(confusion):
   print('\n===== Confusion Matrix =====')
   for i in range(0, 20):
-    sys.stdout.write(f'\t{i}')
+    sys.stdout.write("\t%7d" % i)
 
   print('\n')
 
   label = 0
   for i in confusion:
-    sys.stdout.write(f'{label}\t')
+    sys.stdout.write(f'{label}')
     for j in i:
-      sys.stdout.write(f'{j}\t')
+      sys.stdout.write("\t%7d" % j)
 
     label += 1
     print()
